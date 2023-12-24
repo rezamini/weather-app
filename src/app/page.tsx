4cfdc40a437c343a98ad84723ffc8656c74d2178
Reza <reza.amini.b@gmail.com>
2023-12-24 22:55:34 +0000

    - added sample weather day cards

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,16 @@
 import Header from "./components/header/Header";
+import DayCard from "./components/card/DayCard";
+import { FaSun } from "react-icons/fa";
 
 export default function Home() {
   return (
     // className="flex min-h-screen flex-col items-center justify-between p-24"
     <main>
       <Header />
+      <section className="grid grid-cols-[repeat(auto-fit,100px)] gap-2 justify-center p-4">
+        <DayCard icon={<FaSun className="w-16 h-16"/>} day="Monday" degree={32}/>
+        <DayCard icon={<FaSun className="w-16 h-16"/>} day="Monday" degree={32}/>
+      </section>
     </main>
   );
-}
+} 
