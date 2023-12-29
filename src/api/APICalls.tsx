e6172385e561487f9341dd321bbc7c57a32c8457
Reza <reza.amini.b@gmail.com>
2023-12-29 02:03:42 +0000

    - updated the get weather api method to... retrieve relevant forecast data

--- src/api/APICalls.tsx
+++ src/api/APICalls.tsx
@@ -1,6 +1,34 @@
+//https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current=temperature_2m,weather_code,wind_speed_10m&hourly=temperature_2m,apparent_temperature,precipitation,weather_code,wind_speed_10m&daily=weather_code,temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min,precipitation_sum&precipitation_unit=inch&timeformat=unixtime&timezone=GMT
+"use server";
 
-export default function APICalls() {
-  return (
-    <div>APICalls</div>
-  )
+import axios from "axios";
+
+export async function getWeather(
+  lat: number,
+  lon: number,
+  timezone: string
+): Promise<any> {
+  "use server";
+
+  const resp =  await axios.get(
+    "https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current=temperature_2m,weather_code,wind_speed_10m&hourly=temperature_2m,apparent_temperature,precipitation,weather_code,wind_speed_10m&daily=weather_code,temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min,precipitation_sum&precipitation_unit=inch&timeformat=unixtime&timezone=GMT",
+    {
+      params: {
+        latitude: lat,
+        longitude: lon,
+        timezone,
+      },
+    }
+  );
+
+  return resp.data;
 }
+
+// export async function getTestData(): Promise<any> {
+//   "use server";
+//   const res = await fetch(
+//     "https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current=temperature_2m,weather_code,wind_speed_10m&hourly=temperature_2m,apparent_temperature,precipitation,weather_code,wind_speed_10m&daily=weather_code,temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min,precipitation_sum&precipitation_unit=inch&timeformat=unixtime&timezone=GMT"
+//   );
+//   const data = await res.json();
+//   return data;
+// }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,39 @@
+"use client";
+
 import Header from "../components/header/Header";
 import DayCard from "../components/card/DayCard";
 import { FaSun } from "react-icons/fa";
 import TableRow from "../components/table/TableRow";
+import { useEffect } from "react";
+import { getWeather } from "@/api/APICalls";
+import { log } from "console";
+
 export default function Home() {
+  useEffect(() => {
+    
+    const getData = async () => {
+      const allData = await getWeather(10, 10, Intl.DateTimeFormat().resolvedOptions().timeZone);
+      console.log(allData);
+    }
+    getData();
+    
+  }, []);
+
   return (
     // className="flex min-h-screen flex-col items-center justify-between p-24"
     <main>
       <Header />
       <section className="grid grid-cols-[repeat(auto-fit,100px)] gap-2 justify-center p-4">
-        <DayCard icon={<FaSun className="w-16 h-16"/>} day="Monday" degree={32}/>
-        <DayCard icon={<FaSun className="w-16 h-16"/>} day="Monday" degree={32}/>
+        <DayCard
+          icon={<FaSun className="w-16 h-16" />}
+          day="Monday"
+          degree={32}
+        />
+        <DayCard
+          icon={<FaSun className="w-16 h-16" />}
+          day="Monday"
+          degree={32}
+        />
       </section>
 
       <table className="w-full text-center border-spacing-0">
@@ -18,9 +42,8 @@ export default function Home() {
           <TableRow />
           <TableRow />
           <TableRow />
-          
         </tbody>
       </table>
     </main>
   );
-} 
+}
