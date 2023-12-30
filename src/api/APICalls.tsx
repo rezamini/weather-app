dbbb75e09289b5ef52602b3f389bf1ae75178ca1
Reza <reza.amini.b@gmail.com>
2023-12-30 17:19:54 +0000

    - added a new method to parse the api data for... the current weather

--- src/api/APICalls.tsx
+++ src/api/APICalls.tsx
@@ -10,18 +10,25 @@ export async function getWeather(
 ): Promise<any> {
   "use server";
 
-  const resp =  await axios.get(
-    "https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current=temperature_2m,weather_code,wind_speed_10m&hourly=temperature_2m,apparent_temperature,precipitation,weather_code,wind_speed_10m&daily=weather_code,temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min,precipitation_sum&precipitation_unit=inch&timeformat=unixtime&timezone=GMT",
-    {
-      params: {
-        latitude: lat,
-        longitude: lon,
-        timezone,
-      },
-    }
-  );
+  return await axios
+    .get(
+      "https://api.open-meteo.com/v1/forecast?current=temperature_2m,weather_code,wind_speed_10m&hourly=temperature_2m,apparent_temperature,precipitation,weather_code,wind_speed_10m&daily=weather_code,temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min,precipitation_sum&precipitation_unit=inch&timeformat=unixtime",
+      {
+        params: {
+          latitude: lat,
+          longitude: lon,
+          timezone,
+        },
+      }
+    )
+    .then((response) => {
+      //OR then(({data}))
+      return {
+        current: parseCurrentWeather(response.data),
+      };
+    });
 
-  return resp.data;
+  // return resp.data;
 }
 
 // export async function getTestData(): Promise<any> {
@@ -32,3 +39,30 @@ export async function getWeather(
 //   const data = await res.json();
 //   return data;
 // }
+
+function parseCurrentWeather({ current, daily }: any) {
+  const {
+    temperature_2m: currentTemp,
+    wind_speed_10m: windSpeed,
+    weather_code: iconCode,
+  } = current;
+  const {
+    temperature_2m_max: [maxTemp],
+    temperature_2m_min: [minTemp],
+    apparent_temperature_max: [maxFeelsLike],
+    apparent_temperature_min: [minFeelsLike],
+    precipitation_sum: [precip],
+  } = daily;
+
+  // temperature_2m: [maxTemp] same as const maxTemp = daily.temperature_2m[0]
+  return {
+    currentTemp: currentTemp,
+    highTemp: maxTemp,
+    lowTemp: minTemp,
+    highFeelsLike: maxFeelsLike,
+    lowFeelsLike: minFeelsLike,
+    windSpeed: windSpeed,
+    precip: precip,
+    iconCode: iconCode,
+  };
+}
