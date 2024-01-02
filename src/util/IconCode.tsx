cdb762636f6d95a4121e22967b7b6fcb66f8eaa4
Reza <reza.amini.b@gmail.com>
2024-01-02 12:55:07 +0000

    - added a new utility folder and hierarchy...

---
+++ src/util/IconCode.tsx
@@ -0,0 +1,9 @@
+import { FaSun, FaCloudSun, FaCloud, FaSmog, FaCloudRain, FaCloudMoonRain, FaCloudSunRain, FaSnowflake, FaSnowman, FaBolt, FaCloudShowersHeavy } from "react-icons/fa";
+
+export default function IconCode(code: number) {
+  let IconComponent;
+
+  return "";
+}
+
+
