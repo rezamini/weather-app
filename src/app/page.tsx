c33a278cc7eca05c673bce1bb4e03426f0263246
Reza <reza.amini.b@gmail.com>
2024-01-04 02:18:32 +0000

    - updated the header component to accept props... and related current weather values

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,25 +4,40 @@ import Header from "../components/header/Header";
 import DayCard from "../components/card/DayCard";
 import { FaSun } from "react-icons/fa";
 import TableRow from "../components/table/TableRow";
-import { useEffect } from "react";
-import { getWeather } from "@/api/APICalls";
+import { useEffect, useState } from "react";
+import { getWeather, CurrentWeatherType } from "@/api/APICalls";
 import { log } from "console";
 
 export default function Home() {
+  const [currentData, setCurrentData] = useState<CurrentWeatherType>();
+
   useEffect(() => {
-    
     const getData = async () => {
-      const allData = await getWeather(10, 10, Intl.DateTimeFormat().resolvedOptions().timeZone);
-      console.log(allData);
-    }
+      const allData = await getWeather(
+        10,
+        10,
+        Intl.DateTimeFormat().resolvedOptions().timeZone
+      );
+      console.log(allData.current);
+      setCurrentData(allData.current);
+    };
     getData();
-    
   }, []);
 
   return (
     // className="flex min-h-screen flex-col items-center justify-between p-24"
     <main>
-      <Header />
+      <div>{currentData?.currentTemp}</div>
+      <Header
+        currentTemp={31}
+        highTemp={31}
+        lowTemp={20}
+        highFeelsLike={30}
+        lowFeelsLike={20}
+        windSpeed={1}
+        precip={1}
+        
+      />
       <section className="grid grid-cols-[repeat(auto-fit,100px)] gap-2 justify-center p-4">
         <DayCard
           icon={<FaSun className="w-16 h-16" />}
--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,51 +1,75 @@
 import Image from "next/image";
+import { IconType } from "react-icons";
 import { FaSun } from "react-icons/fa";
 
-export default function Header() {
+type HeaderProps = {
+  currentTemp: number;
+  highTemp: number;
+  lowTemp: number;
+  highFeelsLike: number;
+  lowFeelsLike: number;
+  windSpeed: number;
+  precip: number;
+  Icon?: React.ReactNode; // IconType |  React.ReactNode
+};
+
+export default function Header({ currentTemp = 31, highTemp=32, lowTemp=9, highFeelsLike=30, lowFeelsLike=20, windSpeed=9, precip=0.1, Icon=<FaSun /> }: HeaderProps) {
   return (
     <header className="flex items-center my-4">
       <div className="flex w-1/2 justify-center items-center m-0.5 p-0.5 border-r-2 border-foregroundColor">
-        <FaSun className ="w-20 h-20 object-contain" />
+      {Icon}
         <div className="text-3xl ml-4">
-          <span data-current-temp>31</span>&deg;
+          <span data-current-temp>{currentTemp}</span>&deg;
         </div>
       </div>
       <div className="grid w-1/2 gap-4 justify-around grid-cols-3 grid-rows-2">
         <div className="flex flex-col items-center">
-          <div className="uppercase font-bold text-xs text-foregroundSecondaryColor">High</div>
+          <div className="uppercase font-bold text-xs text-foregroundSecondaryColor">
+            High
+          </div>
           <div>
-            <span data-current-high>32</span>&deg;
+            <span data-current-high>{highTemp}</span>&deg;
           </div>
         </div>
         <div className="flex flex-col items-center">
-          <div className="uppercase font-bold text-xs text-foregroundSecondaryColor">FL High</div>
+          <div className="uppercase font-bold text-xs text-foregroundSecondaryColor">
+            FL High
+          </div>
           <div>
-            <span data-current-fl-high>27</span>&deg;
+            <span data-current-fl-high>{highFeelsLike}</span>&deg;
           </div>
         </div>
         <div className="flex flex-col items-center ">
-          <div className="uppercase font-bold text-xs text-foregroundSecondaryColor">Wind</div>
+          <div className="uppercase font-bold text-xs text-foregroundSecondaryColor">
+            Wind
+          </div>
           <div>
-            <span data-current-wind>9</span>
-            <span className="font-normal text-sm">mph</span> 
+            <span data-current-wind>{windSpeed}</span>
+            <span className="font-normal text-sm">mph</span>
           </div>
         </div>
         <div className="flex flex-col items-center">
-          <div className="uppercase font-bold text-xs text-foregroundSecondaryColor">Low</div>
+          <div className="uppercase font-bold text-xs text-foregroundSecondaryColor">
+            Low
+          </div>
           <div>
-            <span data-current-low>19</span>&deg;
+            <span data-current-low>{lowTemp}</span>&deg;
           </div>
         </div>
         <div className="flex flex-col items-center">
-          <div className="uppercase font-bold text-xs text-foregroundSecondaryColor">FL Low</div>
+          <div className="uppercase font-bold text-xs text-foregroundSecondaryColor">
+            FL Low
+          </div>
           <div>
-            <span data-current-fl-low>12</span>&deg;
+            <span data-current-fl-low>{lowFeelsLike}</span>&deg;
           </div>
         </div>
         <div className="flex flex-col items-center">
-          <div className="uppercase font-bold text-xs text-foregroundSecondaryColor">Precip</div>
+          <div className="uppercase font-bold text-xs text-foregroundSecondaryColor">
+            Precip
+          </div>
           <div>
-            <span data-current-precip>0.1</span>
+            <span data-current-precip>{precip}</span>
             <span className="font-normal text-sm">in</span>
           </div>
         </div>
