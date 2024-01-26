4e4864365fdfbb11674ef651e90245bb4c7fada8
Reza <reza.amini.b@gmail.com>
2024-01-26 10:57:11 +0000

    - added and retrieved the search input value ...

--- src/components/search/Search.tsx
+++ src/components/search/Search.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import SearchResult from "./SearchResult";
 type SearchProps = {
   latitude: number;
@@ -6,15 +6,22 @@ type SearchProps = {
 };
 
 export default function Search({ latitude = 10, longitude = 10 }: SearchProps) {
+  const [searchValue, setSearchValue] = useState<string>("");
+  
   return (
     <div className="flex justify-center m-10 gap-2">
       <div className="flex flex-col">
         <label className="text-xs text-foregroundColor font-bold mb-1">
           Search City
         </label>
-        <input className="rounded-md outline-none p-2 text-sm" value={123} />
+        <input
+          className="rounded-md outline-none p-2 text-sm placeholder:text-xs"
+          placeholder="Input City - Min 3 Characters"
+          value={searchValue}
+          onChange={(e) => setSearchValue(e.target.value)}
+        />
         <div>
-          <SearchResult searchResult={[]} />
+          <SearchResult searchResult={[]}/>
         </div>
       </div>
       <div className="flex flex-col">
