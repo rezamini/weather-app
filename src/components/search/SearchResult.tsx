3c6a801d85ffc5152709bbf2addad205bf8fc7b3
Reza <reza.amini.b@gmail.com>
2024-01-28 14:37:04 +0000

    - added a clickable fragment wrapper for the... result row

--- src/components/search/SearchResult.tsx
+++ src/components/search/SearchResult.tsx
@@ -1,14 +1,31 @@
-import { Fragment, useState } from "react";
+import React, { Fragment, useState } from "react";
 import { CitySearchType } from "@/api/APICalls";
 // type SearchResultProps = {
 
 // } & CitySearchType[];
-export default function SearchResult({ searchResult }: { searchResult: CitySearchType[] }) {
+export default function SearchResult({
+  searchResult,
+}: {
+  searchResult: CitySearchType[];
+}) {
   // const [first, setfirst] = useState([{ id: "1", city:"london", latitude:38.4499712, longitude:27.230208 }, { id: "2", city:"london2", latitude:38.4499712, longitude:27.230208 }, { id: "3", city:"london3", latitude:38.4499712, longitude:27.230208 }]);
- 
+
+  // function onClickResultHandler(item:CitySearchType) {
+  //   alert(item.name);
+  // }
+
+  const FragmentClickWrapper = ({ children, item }: any) => (
+    <React.Fragment>
+      {React.Children.map(children, (child) =>
+        React.cloneElement(child, {
+          // onClick: () => onClickResultHandler(item),
+        })
+      )}
+    </React.Fragment>
+  );
+
   return (
-    
-    <div className={`relative ${searchResult.length > 0 ? 'block': 'hidden'}`}>
+    <div className={`relative ${searchResult.length > 0 ? "block" : "hidden"}`}>
       <div className="bg-gray-300 rounded-md my-2 p-2 bg-opacity-95 absolute left-0 top-0 w-full">
         <div className="grid grid-cols-3 gap-y-2">
           <div className="text-xs font-bold">City</div>
@@ -16,17 +33,15 @@ export default function SearchResult({ searchResult }: { searchResult: CitySearc
           <div className="ml-auto text-xs font-bold">Longitude</div>
           {searchResult.map((item, index) => {
             return (
-              <Fragment key={item.id} >
-                <div className="text-xs text-foregroundColor">
-                  {item.name}
-                </div>
+              <FragmentClickWrapper key={item.id} item={item}>
+                <div className="text-xs text-foregroundColor">{item.name}</div>
                 <div className="ml-auto mr-2 text-xs text-foregroundColor">
-                {Math.round(item.latitude * 100) / 100}
+                  {Math.round(item.latitude * 100) / 100}
                 </div>
                 <div className="mx-auto text-xs text-foregroundColor">
                   {Math.round(item.longitude * 100) / 100}
                 </div>
-              </Fragment>
+              </FragmentClickWrapper>
             );
           })}
         </div>
