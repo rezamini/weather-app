0de14225463168dc81c943ddcfec1576fe0614a3
Reza <reza.amini.b@gmail.com>
2024-02-08 10:44:21 +0000

    Revert "- updated the next js config file"
    This reverts commit 5e827248d3b9fc12fb991dd67f9fcc46c16fbefe.

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: false, //avoid calling the page.tsx twice
-    output: 'export',
 }
 
 module.exports = nextConfig
