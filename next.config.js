5e827248d3b9fc12fb991dd67f9fcc46c16fbefe
Reza <reza.amini.b@gmail.com>
2024-02-08 10:24:14 +0000

    - updated the next js config file

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: false, //avoid calling the page.tsx twice
+    output: 'export',
 }
 
 module.exports = nextConfig
